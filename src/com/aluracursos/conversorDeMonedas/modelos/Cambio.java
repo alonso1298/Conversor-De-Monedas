5349c0d6f88d3b3c406219288c920db98cb838a6
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-06-20 09:29:12 -0600

    Se crea clase Cambio

---
+++ src/com/aluracursos/conversorDeMonedas/modelos/Cambio.java
@@ -0,0 +1,37 @@
+package com.aluracursos.conversorDeMonedas.modelos;
+
+import java.util.Map;
+
+public class Cambio {
+    private String ultimaActualizacion;
+    private String siguienteActualizacion;
+    private String monedaSeleccionada;
+    private Map<String, Double> conversiones;
+
+    public Cambio(CambioRate miCmabio){
+        this.ultimaActualizacion = miCmabio.time_last_update_utc();
+        this.siguienteActualizacion = miCmabio.time_next_update_utc();
+        this.monedaSeleccionada = miCmabio.base_code();
+        this.conversiones = miCmabio.conversion_rates();
+    }
+
+    public String getUltimaActualizacion() {
+        return ultimaActualizacion;
+    }
+
+    public String getSiguienteActualizacion() {
+        return siguienteActualizacion;
+    }
+
+    public String getMonedaSeleccionada() {
+        return monedaSeleccionada;
+    }
+
+    public Map<String, Double> getConversiones() {
+        return conversiones;
+    }
+
+    public void setMonedaSeleccionada(String monedaSeleccionada) {
+        this.monedaSeleccionada = monedaSeleccionada;
+    }
+}
